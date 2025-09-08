c7e3e920f6d680bf4f9a6720899e6f72b9eeb75d
khillon-makwana <khillon2005@gmail.com>
2025-09-08 14:56:30 +0300

    Created an Email object

--- conf.php
+++ conf.php
@@ -9,7 +9,15 @@ $conf['db_type'] = 'pdo';
 $conf['db_host'] = 'localhost';
 $conf['db_user'] = 'root';
 $conf['db_pass'] = '';
-$conf['db_name'] = 'nol';
+$conf['db_name'] = 'IAP-class';
 
 // Site Language
 $conf['site_lang'] = 'en';
+
+// Email Configuration
+$conf['mail_type'] = 'smtp'; // Options: 'smtp' or 'mail'
+$conf['smtp_host'] = 'smtp.gmail.com';
+$conf['smtp_user'] = 'khillon.makwana@strathmore.edu';
+$conf['smtp_pass'] = 'vtet jmpq xeeb pnrk';
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = 'ssl';
