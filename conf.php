e15b1d94a5300d90b4c43908e93aa58e2c1657ae
khillon-makwana <khillon2005@gmail.com>
2025-09-08 14:57:16 +0300

    Created an Email object V2

--- conf.php
+++ conf.php
@@ -17,7 +17,12 @@ $conf['site_lang'] = 'en';
 // Email Configuration
 $conf['mail_type'] = 'smtp'; // Options: 'smtp' or 'mail'
 $conf['smtp_host'] = 'smtp.gmail.com';
+<<<<<<< HEAD
 $conf['smtp_user'] = 'khillon.makwana@strathmore.edu';
 $conf['smtp_pass'] = 'vtet jmpq xeeb pnrk';
+=======
+$conf['smtp_user'] = 'bbitalex@gmail.com';
+$conf['smtp_pass'] = '';
+>>>>>>> 5b26170 (Created an Email object V2)
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
